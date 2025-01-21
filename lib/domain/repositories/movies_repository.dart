7cb846ec7f85c596346bbb05c6277f704f8ef6a7
Juan Kuga <juankp3@gmail.com>
2025-01-21 13:56:49 -0500

    feat: Sección 12: MovieRepository - Implementación

--- lib/domain/repositories/movies_repository.dart
+++ lib/domain/repositories/movies_repository.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_cinemapedia/domain/entities/movie.dart';
 
-abstract class MovieRepository {
+abstract class MoviesRepository {
   Future<List<Movie>> getNowPlaying({int page = 1});
 }
---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:flutter_cinemapedia/domain/entities/movie.dart';
+import 'package:flutter_cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
